6e401662815a16ef6aab59fd6c2a45cdd2324427
Oluwafemijohn <femiogundipe01@gmail.com>
2021-08-09 00:08:06 +0100

    PaddingsAndMargins

--- components/PaddingsAndMargins.js
+++ components/PaddingsAndMargins.js
@@ -14,7 +14,26 @@ export default function PaddingsAndMargins() {
           style={{
             width: 100,
             height: 100,
-            backgroundColor: "dodgerblue"
+            backgroundColor: "dodgerblue",
+            padding: 20,
+
+          }}
+        >
+          <View
+          style={{
+            backgroundColor: "gold",
+            width: 50,
+            height: 50,
+          }}
+          ></View>
+        </View>
+        <View
+          style={{
+            width: 100,
+            height: 100,
+            backgroundColor: "tomato",
+            padding: 20,
+            margin: 20,
           }}
         ></View>
       </View>

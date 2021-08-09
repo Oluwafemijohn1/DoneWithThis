82757e84088d46a135f445f9462ee7798dfeef32
Oluwafemijohn <femiogundipe01@gmail.com>
2021-08-10 00:34:36 +0100

    file organisation

--- components/practiceComponent/EncapsulatingStyles.js
+++ components/practiceComponent/EncapsulatingStyles.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native';
 
-import AppText from '../style_encapsulation/platformSpecific/AppText';
+import AppText from '../style_encapsulation/AppText';
+
 
 
 export default function EncapsulatingStyles() {
--- components/style_encapsulation/AppText.js
+++ components/style_encapsulation/AppText.js
@@ -1,15 +1,11 @@
 import React from 'react'
 import { Text, Platform, StyleSheet } from 'react-native'
 
+import styles from '../style/styles'
+
 export default function AppText(props) {
     return (
        <Text style={styles.text} >{props.children}</Text>
     )
 }
 
-const styles = StyleSheet.create({
-     text:{
-         fontSize: 18,
-         fontFamily: Platform.OS === "android" ? "Roboto" : "Avenir"
-     }
-})

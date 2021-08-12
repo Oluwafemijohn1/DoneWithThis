21b4c8dd9760cab39546e4c3712491e8b1c7a67e
Oluwafemijohn <femiogundipe01@gmail.com>
2021-08-12 17:41:13 +0100

    Did FlatList

--- App.js
+++ App.js
@@ -9,10 +9,11 @@ import WelcomeScreen from "./app/screens/WelcomeScreen";
 import CardComponent from "./components/practiceComponent/CardComponent";
 import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
+import MessagesScreen from "./app/screens/MessagesScreen";
 
 export default function App() {
   return (
-    <ViewImageScreen />
+    <MessagesScreen />
   );
 }
 
